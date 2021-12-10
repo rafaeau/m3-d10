01baad687c2829562ce0a97c710255048feeb0bf
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-12-10 16:53:49 +0100

    Edit feature working

--- back-office.js
+++ back-office.js
@@ -1,5 +1,30 @@
-const url = "https://striveschool-api.herokuapp.com/api/movies"
-const method = "POST"
+const id = new URLSearchParams(location.search).get("id")
+const url = id ? "https://striveschool-api.herokuapp.com/api/movies/" + id : "https://striveschool-api.herokuapp.com/api/movies/"
+const method = id ? "PUT" : "POST"
+
+window.onload = () => {
+
+    if (id) {
+      fetch(url, {
+        method: method,
+        body: JSON.stringify(),
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTRiMjFmMTRiYjUzZDAwMTViMTllZDciLCJpYXQiOjE2Mzg5NzI0MTQsImV4cCI6MTY0MDE4MjAxNH0.oauW9EyjgVFdXEkIv_2CZMaftq-xHesD6DsOGURaFg0"
+        }
+      })
+        .then(response => response.json())
+        .then(productData => {
+          const { name, description, category, imageUrl } = productData
+          document.getElementById("name").value = name
+          document.getElementById("description").value = description
+          document.getElementById("category").value = category
+          document.getElementById("imageUrl").value = imageUrl
+          document.querySelector(".submit-btn").innerText = "Submit changes"
+          document.querySelector(".delete-btn").classList.remove("d-none")
+        })
+    }
+  }
 
 const addFilm = async (event) => {
     event.preventDefault()
@@ -33,3 +58,5 @@ const addFilm = async (event) => {
         console.error(err)
     }
   }
+
+  
