2cf9469fe8e61d883f6a93f1e76539f406c1a127
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-12-10 17:41:14 +0100

    Background color

--- netflix-home.js
+++ netflix-home.js
@@ -1,8 +1,8 @@
 window.onload = () => {
-    displayContent()
+    displayDrama()
 }
 
-const displayContent = async () => {
+const displayDrama = async () => {
     try {
         const response = await fetch("https://striveschool-api.herokuapp.com/api/movies/", {
             method: "GET",
@@ -39,19 +39,39 @@ const displayContent = async () => {
     }
 }
 
+const displayRomance = async () => {
+    try {
+        const response = await fetch("https://striveschool-api.herokuapp.com/api/movies/", {
+            method: "GET",
+            headers: {
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTRiMjFmMTRiYjUzZDAwMTViMTllZDciLCJpYXQiOjE2Mzg5NzI0MTQsImV4cCI6MTY0MDE4MjAxNH0.oauW9EyjgVFdXEkIv_2CZMaftq-xHesD6DsOGURaFg0"
+            }
+        })
 
-
-
-
-
-/* filmArray.forEach(film => {
-    const list = document.querySelector(".list-group")
-    document.querySelector(".spinner-border").classList.add("d-none")
-    console.log(film)
-    const item = document.createElement("li")
-    item.classList.add("list-group-item", "d-flex", "align-items-center")
-    item.innerHTML = `<img src="${film.imageUrl}" height="60px" alt="product-photo">
-    <span class="ml-2">Name: ${film.name}<br>Category: ${film.category}<br>Description: ${film.description}</span>`
-    list.appendChild(item)
-    <h5><a href="./detail-page.html?id=${film.id}"><span class="badge badge-lg badge-info ml-3">Details</span></a></h5>
-}) */
+        if (response.ok) {
+            const categories = await response.json()
+            console.log(categories)
+            const films = await Promise.all(
+                categories.map(async (category) => {
+                    const res = await fetch("https://striveschool-api.herokuapp.com/api/movies/" + category, {
+                        method: "GET",
+                        headers: {
+                            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTRiMjFmMTRiYjUzZDAwMTViMTllZDciLCJpYXQiOjE2Mzg5NzI0MTQsImV4cCI6MTY0MDE4MjAxNH0.oauW9EyjgVFdXEkIv_2CZMaftq-xHesD6DsOGURaFg0"
+                        }
+                    })
+                    return await res.json()
+                })
+            )
+            films[1].forEach(film => {
+                const list = document.getElementById("romance")
+                const item = document.createElement("div")
+                item.classList.add("img-wrap", "col-sm-6", "col-md-4", "col-lg-2", "mb-1", "pr-0")
+                item.innerHTML = `<a href="./detail-page.html?id=${film._id}"><img class="image-card img-fluid" height="200" src=${film.imageUrl} href="./detail-page.html?id=${film._id}"/></a>`
+                list.appendChild(item)
+                console.log(film)
+            })
+        }
+    } catch (err) {
+        console.error(err)
+    }
+}
